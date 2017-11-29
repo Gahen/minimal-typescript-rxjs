58c2d2d9be17d594d446675bfff15aa7e3159e65
Joaquín Omar Tumas <j.tumas@gmail.com>
2017-11-29 16:16:56 -0300

    tmp

--- index.ts
+++ index.ts
@@ -9,20 +9,18 @@ stdin.setEncoding( 'utf8' );
 // on any data into stdin
 const inputStream = Rx.Observable.fromEvent(stdin, 'data')
 
-const startingReqStream = Rx.Observable.of('https://api.github.com/users')
-
 var interactiveReqStream = inputStream
   .map(function() {
     var randomOffset = Math.floor(Math.random()*500)
     return 'https://api.github.com/users?since=' + randomOffset
   })
+  .startWith('https://api.github.com/users')
 
-const requestStream = Rx.Observable.merge(startingReqStream, interactiveReqStream)
-
-var responseStream = requestStream.flatMap(url => {
+var responseStream = interactiveReqStream.flatMap(url => {
     const textedResponse = fetch(url).then(res => res.text())
     return Rx.Observable.fromPromise(textedResponse)
-}).subscribe(response => {
+})
+.subscribe(response => {
     console.log(response)
 })
 
