c28e8fb9f5ed611f46eec954c66c1fcd8fa87b6d
Joaquín Omar Tumas <j.tumas@gmail.com>
2017-11-30 04:05:47 -0300

    final example, without an extra function call

--- index.ts
+++ index.ts
@@ -6,17 +6,27 @@ var stdin = process.stdin;
 stdin.resume();
 stdin.setEncoding( 'utf8' );
 
+const getRand = listUsers => listUsers[Math.floor(Math.random()*listUsers.length)]
+
 // on any data into stdin
-const inputStream = Rx.Observable.fromEvent(stdin, 'data')
+const inputStream: Rx.Observable<string> = Rx.Observable.fromEvent(stdin, 'data')
 
-# const inputStreamClose = inputStream.filter(r => r === 'x')
-const inputStreamElse = inputStream.filter(r => r !== 'x')
+const inputStream1Close = inputStream.filter(r => r.indexOf('x1') !== -1)
+const inputStream2Close = inputStream.filter(r => r.indexOf('x2') !== -1)
+const inputStream3Close = inputStream.filter(r => r.indexOf('x3') !== -1)
+const inputStreamRefresh = inputStream.filter(r => r.indexOf('x') === -1)
 
-var interactiveReqStream = inputStreamElse
+// inputStreamRefresh.subscribe(() => console.log('refreshing!'))
+// inputStream.subscribe(() => console.log('input detected!'))
+
+const username = process.argv[2]
+const pass = process.argv[3]
+
+var interactiveReqStream = inputStreamRefresh
   .startWith('startup click')
   .map(() => {
     var randomOffset = Math.floor(Math.random()*500)
-    return 'https://api.github.com/users?since=' + randomOffset
+    return `https://${username}:${pass}@api.github.com/users?since=${randomOffset}`
   })
 
 var responseStream: Rx.Observable<any[]> = interactiveReqStream.flatMap(url => {
@@ -24,20 +34,24 @@ var responseStream: Rx.Observable<any[]> = interactiveReqStream.flatMap(url => {
     return Rx.Observable.fromPromise(jsonResponse)
 })
 
-var suggestion1Stream = responseStream.map(listUsers => {
-    return listUsers[Math.floor(Math.random()*listUsers.length)]
-})
+var suggestion1Stream = inputStream1Close
+.startWith('startup click')
+.combineLatest(responseStream, (click, listUsers) => getRand(listUsers))
 .merge(interactiveReqStream.map(() => null))
+// .startWith(null)
 .subscribe(r => console.log('stream 1', r))
 
-var suggestion2Stream = responseStream.map(listUsers => {
-    return listUsers[Math.floor(Math.random()*listUsers.length)]
-})
+var suggestion2Stream = inputStream2Close
+.startWith('startup click')
+.combineLatest(responseStream, (click, listUsers) => getRand(listUsers))
 .merge(interactiveReqStream.map(() => null))
+// .startWith(null)
 .subscribe(r => console.log('stream 2', r))
 
-var suggestion3Stream = responseStream.map(listUsers => {
-    return listUsers[Math.floor(Math.random()*listUsers.length)]
-})
+var suggestion3Stream = inputStream3Close
+.startWith('startup click')
+.combineLatest(responseStream, (click, listUsers) => getRand(listUsers))
 .merge(interactiveReqStream.map(() => null))
+// .startWith(null)
 .subscribe(r => console.log('stream 3', r))
+
